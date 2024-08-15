505e44629f70fa77e140e5c65ac52932f6faaab1
TilakKhatri <tilakkhatri0001@gmail.com>
2024-08-16 00:29:51 +0545

    Feat: update user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -85,6 +85,92 @@ class UserControllers {
       return res.apiError("Something went wrong", 500);
     }
   };
+
+  getUserById = async (req, res) => {
+    try {
+      const userId = req.params.id;
+      const userResult = await db.query(
+        `SELECT id, first_name,last_name,email,email,phone,address,gender,isAdmin FROM users WHERE id = $1`,
+        [userId]
+      );
+      // Check if a user was found
+      if (userResult.rows.length === 0) {
+        return res.apiError("User not found", 404);
+      }
+      const userDetail = userResult.rows[0];
+      return res.apiSuccess("User Detail Fetching", { userDetail }, 200);
+    } catch (error) {
+      return res.apiError("Something went wrong", 500);
+    }
+  };
+
+  deleteUserById = async (req, res) => {
+    try {
+      const userId = req.params.id;
+      const userResult = await db.query(
+        `DELETE FROM users where id = $1 RETURNING id`,
+        [userId]
+      );
+      console.log(userResult);
+      if (userResult.rows.length === 0) {
+        return res.apiError("User not found", 404);
+      }
+
+      return res.apiSuccess("User deleted successfully", null, 200);
+    } catch (error) {
+      return res.apiError("Something went wrong", 500);
+    }
+  };
+
+  editUserById = async (req, res) => {
+    try {
+      const userId = req.params.id;
+      const {
+        first_name,
+        last_name,
+        email,
+        phone,
+        dob,
+        gender,
+        address,
+        isAdmin,
+      } = req.body;
+
+      const existingUserResult = await db.query(
+        "SELECT * FROM users WHERE id = $1",
+        [userId]
+      );
+
+      if (existingUserResult.rows.length === 0) {
+        return res.apiError("User not found", 404);
+      }
+
+      const userResult = await db.query(
+        `UPDATE users SET first_name = $1, last_name = $2, email = $3, phone = $4, dob = $5, gender = $6, address = $7, isAdmin = $8, updated_at = $9
+             WHERE id = $10 RETURNING id, first_name, last_name, email, phone, dob, gender, address, isAdmin`,
+
+        [
+          first_name,
+          last_name,
+          email,
+          phone,
+          dob,
+          gender,
+          address,
+          isAdmin,
+          new Date(),
+          userId,
+        ]
+      );
+      console.log(userResult);
+
+      const updateUser = userResult.rows[0];
+      return res.apiSuccess("Fetching user after update", { updateUser }, 200);
+      return;
+    } catch (error) {
+      return res.apiError(`Something went wrong ${error.message}`, 500);
+    }
+  };
 }
 
 export default UserControllers;
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,5 +4,8 @@ import UserControllers from "../controllers/user.controller.js";
 const router = Router();
 const userControllers = new UserControllers();
 router.get("", userControllers.getAllUsers);
+router.get("/:id", userControllers.getUserById);
+router.delete("/:id", userControllers.deleteUserById);
+router.put("/:id", userControllers.editUserById);
 
 export default router;
